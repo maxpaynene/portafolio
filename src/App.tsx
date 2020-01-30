5a1645e54cd483c73494aa3d0049a69484b092e2
Mozart Catrina <mozartcatrina@MacBook-Air-de-Mozart.local>
2020-01-30 13:00:36 -0300

    test video bypass  CORS

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import AxiosPromise from './Component/AxiosPromise';
 
 const App: React.FC = () => {
+
   return (
     <div className="App">
+      <video width="400" loop autoPlay>
+        <source src="//player.vimeo.com/video/174002648?title=0&portrait=0&byline=0&autoplay=1" type="video/mp4"/>
+        Your browser does not support HTML5 video.
+      </video>
       <header className="App-header">
         <AxiosPromise/>
       </header>
     </div>
   );
+  
 }
 
 export default App;
