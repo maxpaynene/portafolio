9a8ec261765caf47e18a8a03aad92f04467a40bc
Mozart Catrina <mozartcatrina@MacBook-Air-de-Mozart.local>
2020-01-30 13:24:12 -0300

    test video - viendo si el video se logra cargar y  si es posible ver el rendimiento

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/08fa0216-eb2d-4511-aee8-dedf6d87edd3/deploy-status)](https://app.netlify.com/sites/maxpaynene-portafolio/deploys)
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,11 @@ import AxiosPromise from './Component/AxiosPromise';
 const App: React.FC = () => {
 
   return (
-    <div className="App">
-      <video width="400" loop autoPlay>
-        <source src="//player.vimeo.com/video/174002648?title=0&portrait=0&byline=0&autoplay=1" type="video/mp4"/>
+    <div>
+      <video loop autoPlay muted className="videoCss">
+        <source src="/video/video.mp4" type="video/mp4"/>
         Your browser does not support HTML5 video.
       </video>
-      <header className="App-header">
-        <AxiosPromise/>
-      </header>
     </div>
   );
   
