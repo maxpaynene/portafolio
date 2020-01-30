f7409dc2a417a60fe4adbfa814f4947f2cb8ca26
Mozart Catrina <mozartcatrina@MacBook-Air-de-Mozart.local>
2020-01-30 15:57:00 -0300

    - Se selecciono el video. - Se puso un tipo de letra en react para el logo. - Se creo un componente para este new logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
-import AxiosPromise from './Component/AxiosPromise';
+import AxiosPromise from './Component/logo/AxiosPromise';
 
 const App: React.FC = () => {
 
   return (
     <div>
-      <video loop autoPlay muted className="videoCss">
-        <source src="/video/video.mp4" type="video/mp4"/>
-        Your browser does not support HTML5 video.
-      </video>
+      <div>
+        <AxiosPromise/>
+      </div>    
+      <div>        
+        {/* Video background */}
+        <video loop autoPlay muted className="videoCss">
+          <source src="/video/video3.mp4" type="video/mp4"/>
+          Your browser does not support HTML5 video.
+        </video>
+      </div>
     </div>
+    
   );
   
 }
---
+++ src/Component/logo/AxiosPromise.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./font.css";
+
+export default class AxiosPromise extends React.Component {
+        
+    render(){
+        return (
+            <text className="fontStyle">
+                Mozart Portafolio
+            </text>                
+        );
+    }
+
+}
